be754c4b750eed508747b8b64592dfdafdf4fd38
Anurag Kumar <anuragak@users.noreply.github.com>
2018-06-04 18:02:48 +0100

    finished basic requirements

---
+++ script.js
@@ -0,0 +1,105 @@
+
+const wrapper = document.querySelector('.wrapper');
+const modal = document.querySelector('.modal');
+
+function fetchData(url) {
+    return fetch(url)
+    .then(checkStatus)
+    .then(res => res.json())
+    .catch(error => console.log('Looks like there was an error!', error));
+}
+
+fetchData('https://randomuser.me/api/?results=12')
+.then(data => fillData(data.results));
+
+function checkStatus(response){
+  if (response.ok){
+      return Promise.resolve(response);
+  } else {
+      return Promise.reject(new Error(response.statusText));
+  }
+}
+
+function fillData(resultList){
+  let images = [];
+  let modals = [];
+  let count = 0;
+  resultList.forEach(result=>{
+  images +=  `
+  <div class="card-contents" id="card-${count}">
+    <img class = "card-profile-img" src=${result.picture.large} alt=''>
+    <div class="card-body">
+      <h3 class="card-name">${result.name.first} ${result.name.last}</h3>
+      <p class="card-email">${result.email}</p>
+      <p class="card-city">${result.location.city}</p>
+    </div>
+  </div>`
+
+  modals += `
+  <div class="modal-content" id="modal-${count}">
+    <span class="closeBtn">&times;</span>
+    <i class="fas fa-caret-left left-arrow"></i>
+    <i class="fas fa-caret-right right-arrow"></i>
+    <div class="modal-body">
+      <img class = "modal-profile-img" src=${result.picture.large} alt=''>
+      <h2 class="modal-name">${result.name.first} ${result.name.last}</h2>
+      <p class="modal-username">${result.login.username}</p>
+      <p class="modal-email">${result.email}</p>
+      <br><hr><br>
+      <p class="modal-city">${result.location.street}, ${result.location.city},
+       ${result.location.state}, ${result.location.postcode}</p>
+      <p class="modal-cell">${result.phone}</p>
+      <p class="modal-birth">Birthday: ${result.dob.split(' ')[0]}</p>
+    </div>
+  </div>`
+  count += 1;
+  }); //end of forEach loop
+  wrapper.innerHTML = images;
+  modal.innerHTML = modals;
+  cardEvents();
+  exitModalEvents();
+  arrowEvents();
+};
+
+function cardEvents(){
+  const cardContents = document.querySelectorAll(".card-contents");
+  cardContents.forEach(card=>
+    card.addEventListener('click', (e) => {
+      const cardNumber = e.currentTarget.id.split('-')[1];
+      modal.style.display = 'block';
+      document.querySelector(`#modal-${cardNumber}`).style.display='grid';
+    })
+  );
+};
+
+function exitModalEvents(){
+  document.querySelectorAll('.closeBtn').forEach(closeButton =>
+    closeButton.addEventListener('click', (e) =>{
+      e.currentTarget.parentElement.style.display='none';
+      modal.style.display = 'none';
+    })
+  );
+}
+
+function arrowEvents(){
+  document.querySelectorAll('.right-arrow').forEach(rightArrow =>
+    rightArrow.addEventListener('click', (e) =>{
+      let currentModal = e.currentTarget.parentElement.id.split('-')[1];
+      e.currentTarget.parentElement.style.display='none';
+      if(currentModal==11){currentModal = -1};
+      document.querySelector(`#modal-${Number(currentModal) + 1}`).style.display='grid';
+    })
+  );
+  document.querySelectorAll('.left-arrow').forEach(leftArrow =>
+    leftArrow.addEventListener('click', (e) =>{
+      let currentModal = e.currentTarget.parentElement.id.split('-')[1];
+      e.currentTarget.parentElement.style.display='none';
+      if(currentModal==0){currentModal = 12};
+      document.querySelector(`#modal-${Number(currentModal) - 1}`).style.display='grid';
+    })
+  );
+}
+
+document.querySelector('#search').addEventListener('keyup', ()=>{
+
+});
